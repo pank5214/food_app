52ed65ee6b6a55bff69af8059821e82dc72be059
pank5214 <pankajkumar5214@gmail.com>
2023-08-04 00:49:51 +0530

    episode 02

--- App.js
+++ App.js
@@ -1,21 +1,10 @@
-/*
- *   <div id="parent">
- *    <div id="child1">
- *        <h1>This is h1 tag</h1>
- *        <h2>This is h2 tag</h2>
- *   </div>
- *    <div id="child2">
- *        <h1>This is h1 tag</h1>
- *        <h2>This is h2 tag</h2>
- *   </div>
- *  </div>
- *
- *  */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "This is h1 tag"),
-    React.createElement("h2", {}, "This is h2 tag"),
+    React.createElement("h1", {}, "This is Namaste React 🚀"),
+    React.createElement("h2", {}, "Created by me!"),
   ]),
   React.createElement("div", { id: "child2" }, [
     React.createElement("h1", {}, "This is h1 tag"),
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# namaste-react
+# Namaste React 🚀
 React projects
+
+
+# Parcel is a beast:
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watching Algorithm ---> Written in c++
+- Caching ---> Makes Faster Builsd
+- Image Optimization
+- Minification
+- Bundling
+- Compress File
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling ----> Support Older Browser
+- Diagnostics
+- Error Handling
+- HTTPs
+- Tree Shaking ---> Remove unused Code
+- Creates Different dev & Production bundles
