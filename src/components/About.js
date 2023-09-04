a91e27a94b699cd5d59985af5b7f6862f57207f8
pank5214 <pankajkumar5214@gmail.com>
2023-09-04 23:22:05 +0530

    make app responsive

--- src/components/About.js
+++ src/components/About.js
@@ -3,9 +3,9 @@ import food from "../../Images/burger-image.png";
 
 const About = () => {
   return (
-    <div className="flex justify-between m-4 p-4">
-      <div className="self-center">
-        <h1 className="font-bold text-4xl my-4  text-green-900">
+    <div className="flex flex-col md:flex-row md:justify-between p-4 m-4">
+      <div className="mx-15 md:mx-0 md:self-center">
+        <h1 className="font-bold text-3xl md:text-4xl my-4 text-green-900">
           Welcome to the
           <br />
           <br />
@@ -16,12 +16,16 @@ const About = () => {
             Tasty and Fresh Creations!
           </span>
         </h1>
-        <h4 className="text-lg font-style: italic text-slate-500">
+        <h4 className="md:text-lg font-style: italic text-slate-500">
           "Fueling Your Day with FoodHunger's Healthy Delight!"
         </h4>
       </div>
-      <div className="">
-        <img src={food} alt="Food Image" />
+      <div className="flex justify-center">
+        <img
+          className="w-[55%] h-[180px] my-12 md:m-0 md:w-auto md:h-auto"
+          src={food}
+          alt="Food Image"
+        />
       </div>
     </div>
   );
