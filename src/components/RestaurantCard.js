d03d4b2ffaeb81640ea462959f4aea15b23f840d
pank5214 <pankajkumar5214@gmail.com>
2023-12-22 12:36:08 +0530

    res card dimensions work properly

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -8,26 +8,36 @@ const RestaurantCard = (props) => {
   return (
     <div
       data-testid="resCard"
-      className="m-2 p-3 md:m-4 md:p-4 w-[250px] h-[400px] rounded-lg bg-gray-100 hover:bg-gray-200"
+      className="grid m-2 p-3 md:m-3 md:p-4 w-[255px] h-[350px] rounded-lg bg-gray-100 hover:bg-gray-200"
     >
       <img
         className="rounded-lg"
         src={CDN_URL + cloudinaryImageId}
         alt="res-logo"
       />
-      <h3 className="font-semibold py-3">{name}</h3>
-      <h4 className="pb-1">
+      <h3 className="font-semibold pt-3 line-clamp-1">{name}</h3>
+      <h4 className="pb-1 pt-2 line-clamp-2">
         {cuisines && cuisines.length > 0
           ? cuisines.join(", ")
           : "No cuisines available"}
       </h4>
 
-      <h4>Rating: {avgRating} stars</h4>
-      <h4 className="py-1">Cost: {costForTwo} </h4>
+      <h4>
+        <span className="font-medium">Rating:</span> {avgRating} stars
+      </h4>
+      <h4 className="py-1">
+        <span className="font-medium">Cost:</span> {costForTwo}{" "}
+      </h4>
       {resData && resData.sla && resData.sla.deliveryTime !== undefined ? (
-        <h4>Delivery Time: {resData.sla.deliveryTime} minutes</h4>
+        <h4>
+          {" "}
+          <span className="font-medium">Delivery Time:</span>{" "}
+          {resData.sla.deliveryTime} minutes
+        </h4>
       ) : (
-        <h4>Delivery Time: N/A</h4>
+        <h4>
+          <span className="font-medium">Delivery Time:</span> N/A
+        </h4>
       )}
     </div>
   );
