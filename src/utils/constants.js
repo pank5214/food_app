d85a28b3faf43b9b7e4de3e04904d67e36dff172
pank5214 <pankajkumar5214@gmail.com>
2024-01-29 21:09:33 +0530

    corsproxy working

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,10 +5,10 @@ export const LOGO_URL =
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSV5p2gG6I22T0CeBb6-Pt7fgTTTk4bTYVlTQ&usqp=CAU";
 
 export const RESTAURANT_API =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING";
+  "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING";
 
 export const MENU_API =
-  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=";
+  "https://corsproxy.io/?https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=12.9351929&lng=77.62448069999999&restaurantId=";
 
 // Social Media Links
 export const Linkedin_Link = "https://www.linkedin.com/in/pankaj5214/";
