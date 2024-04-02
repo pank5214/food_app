2825fa73ecb81e67311571ed1c00ea86c8b986d7
pank5214 <pankajkumar5214@gmail.com>
2024-04-02 09:02:45 +0530

    api resolve regarding restaurantMenu

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -8,16 +8,17 @@ const RestaurantMenu = () => {
   const { resId } = useParams();
 
   const resInfo = useRestaurantMenu(resId);
+  console.log("Res Info: ", resInfo);
 
   const [showIndex, setShowIndex] = useState(null);
 
   if (resInfo === null) return <Shimmer />;
 
   const { name, costForTwoMessage, cuisines } =
-    resInfo?.cards[0].card?.card?.info;
+    resInfo?.cards[2].card?.card?.info;
 
   const categories =
-    resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.filter(
+    resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.filter(
       (c) =>
         c?.card?.card?.["@type"] ===
         "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
